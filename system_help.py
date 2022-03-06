30f7773623f1d9b4b3b9825aebf5b6ac02dbb0a9
Arshpreet Singh <arsharsh2391@gmail.com>
2022-03-07 02:32:47 +0530

    run as admin

--- system_help.py
+++ system_help.py
@@ -1,4 +1,5 @@
-
+import sys
+import ctypes
 import win32api #this is used to get the current user , the current time and the total time workded.
 import win32file
 import win32profile
@@ -16,16 +17,6 @@ def write_to_file(inp):
     with open(FILEPATH,'a') as file:
         file.write(inp)
 
-def update_temp(today_date='NA',
-                machine_name='NA',
-                user_name='NA',
-                worked_hours='NA',
-):
-    '''Updates the inputed kwarg into the string'''
-
-    template = f'For Day : {today_date}\nMachine Name : {machine_name} \nUser Name : {user_name} \nWorked Hours : {worked_hours} \nSystem Info : {system_info} \nSecuriy Identification Key : {sik}'
-    return template
-
 def get_date():
     wdate = win32api.GetSystemTime()
     date = f'{wdate[1]}-{wdate[3]}-{wdate[0]}'
@@ -35,13 +26,18 @@ def get_time_worked():
     time_ms = win32api.GetTickCount()
     time_min = (time_ms/(1000*60))%60
     time_hr = (time_ms/(1000*60*60))%24
-    return f'{time_hr} Hours and {time_min} Minutes'
+    return f'{int(time_hr)} Hours and {int(time_min)} Minutes'
 
 def get_SIK():
     info = (win32security.GetFileSecurity(FILEPATH),win32security.OWNER_SECURITY_INFORMATION)
     return info.GetSecurityDescriptorOwner()
 
-if __name__ == '__main__':
+def window_message():
+    win32gui.MessageBeep(1)
+    message = f'Your Attendance Has Been Marked For Today! Updated Info stored at {FILEPATH}'
+    win32gui.MessageBox(0,message,'System Administrator',0)
+
+def main():
     check_file_exists()
     username = win32api.GetUserName()
     machine_name = win32api.GetComputerName()
@@ -49,8 +45,24 @@ if __name__ == '__main__':
     worked_hours = get_time_worked()
     system_info = win32api.GetSystemInfo()
     sik = get_SIK()
+    #sik = None
 
     template = f'For Day : {today_date}\nMachine Name : {machine_name} \nUser Name : {username} \nWorked Hours : {worked_hours} \nSystem Info : {system_info} \nSecuriy Identification Key : {sik}'
+    write_to_file(template)
+    write_to_file('\n\n')
+    window_message()
 
-    ### Total Time the user worked for ###
-    
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if __name__ == '__main__':
+    if is_admin():
+        main()
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
+# if  __name__ == '__main__':
+#     main()
